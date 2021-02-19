e80c777d5dc0ab33dd5f64d45f5fa8c4cae8fcfb
Avneesh Agarwal <mamtajalan22@gmail.com>
2021-02-19 13:59:06 +0530

    functional component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { Component } from "react";
-class App extends Component {
-  render() {
-    
-    return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    );
-  }
+import "./App.css";
+
+// Functional component
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
