471ef25993c21573d0fbec781a8fa3227856a987
Avneesh Agarwal <mamtajalan22@gmail.com>
2021-02-19 13:03:40 +0530

    introduction to jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const isMale = true;
+
+  function getGreeting(user) {
+    if (user) {
+      return <h1>Hello, {user}</h1>;
+    } else {
+      return <h1>Hello, Stranger</h1>;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {getGreeting("Avneesh")}
+      <h2>You are a {isMale ? "Male" : "Female"}</h2>
     </div>
   );
 }
